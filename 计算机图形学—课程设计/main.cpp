4a8eb7a454019ee4ad8c67e28f6f2ced8c9d8d75
修宇亮 <Personals@osamusakaisuketekiMacBook-Pro.local>
2013-10-14 22:36:51 +0800

    Initial Commit

---
+++ 计算机图形学—课程设计/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  计算机图形学—课程设计
+//
+//  Created by 修宇亮 on 13-10-14.
+//  Copyright (c) 2013年 修宇亮. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
